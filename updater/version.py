19c876a87b423d83d4eb88d5b151ac5882f25db8
red <arui939@gmail.com>
2026-02-16 02:34:10 +0200

    Update V0.1.4 - Fixed Updater

--- updater/version.py
+++ updater/version.py
@@ -1,4 +1,4 @@
 GAME_NAME = "Red's Garbage Game"
-VERSION = "0.1.3"
+VERSION = "0.1.4"
 UPDATE_URL = "https://updates.r3dwolfie.com/api/update"
 VERSION_URL = "https://updates.r3dwolfie.com/api/version"
